c4d020c08dd21e7b99985de41412a64e07901add
Aaryan Kulkarni <aaryanyk@gmail.com>
2026-08-27 15:26:29 +0000

    rough model is made

---
+++ src/manipulator_description/launch/display.launch.py
@@ -0,0 +1,75 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+
+    package_name = 'manipulator_description'
+
+    package_share = get_package_share_directory(package_name)
+
+    xacro_file = os.path.join(
+        package_share,
+        'urdf',
+        'manipulator.xacro'
+    )
+
+    rviz_config_file = os.path.join(
+        package_share,
+        'config',
+        'display.rviz'
+    )
+
+    use_sim_time = LaunchConfiguration('use_sim_time')
+
+    return LaunchDescription([
+
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use simulation time'
+        ),
+
+        # Process Xacro and publish the robot description
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
+            name='robot_state_publisher',
+            output='screen',
+            parameters=[
+                {
+                    'robot_description': Command([
+                        'xacro ',
+                        xacro_file
+                    ]),
+                    'use_sim_time': use_sim_time
+                }
+            ]
+        ),
+
+        # Provides sliders for the revolute joints
+        Node(
+            package='joint_state_publisher_gui',
+            executable='joint_state_publisher_gui',
+            name='joint_state_publisher_gui',
+            output='screen'
+        ),
+
+        # RViz visualization
+        Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz2',
+            output='screen',
+            arguments=[
+                '-d',
+                rviz_config_file
+            ]
+        )
+    ])
